8ae94591a93947d877bb1d7072310170786c1884
Zack Siri <me@zacksiri.com>
2025-07-18 20:28:23 +0700

    Add output for source id

--- modules/inference-service/outputs.tf
+++ modules/inference-service/outputs.tf
@@ -2,3 +2,8 @@ output "model_ids" {
   description = "Map of model identifiers to their IDs"
   value       = { for k, v in tama_model.this : k => v.id }
 }
+
+output "source_id" {
+  description = "ID of the source"
+  value       = tama_source.this.id
+}
