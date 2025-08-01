83d1968063d500656a04f6edcf334e815863bc4f
Zack Siri <me@zacksiri.dev>
2025-08-01 13:38:12 +0700

    Simplify messaging module removing class_proxy_class_id var

--- modules/messaging/variables.tf
+++ modules/messaging/variables.tf
@@ -1,8 +1,3 @@
-variable "class_proxy_class_id" {
-  type        = string
-  description = "The ID of the class proxy class"
-}
-
 variable "name" {
   type        = string
   description = "The name of the corpus"
